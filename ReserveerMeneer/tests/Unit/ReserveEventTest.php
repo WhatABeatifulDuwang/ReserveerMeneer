322c057c5e2b302198a726a554ef2b7943eeddc2
Milan <m.vanoeveren1@student.avans.nl>
2021-06-02 22:22:20 +0200

    setup

---
+++ ReserveerMeneer/tests/Unit/ReserveEventTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Requests\ReserveEventRequest;
+use App\Http\Requests\ReserveRestaurantRequest;
+use Faker\Factory;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class ReserveEventTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+    /**
+     * @dataProvider provideValidData
+     *
+     */
+    public function test_valid_firstname(array $data)
+    {
+        $request = new ReserveEventRequest();
+        $validator = Validator::make($data, $request->rules());
+
+        $this->assertTrue($validator->passes());
+    }
+
+    public function provideValidData(): array
+    {
+        $faker = Factory::create(Factory::DEFAULT_LOCALE);
+
+        return [
+            [[
+                "name" => $faker->title,
+                "email" => $faker->email,
+                "ticket_number" =>  $faker->randomDigit + 1,
+                "start_date" => '07-07-2021',
+                "days_count" => $faker->randomDigit + 1,
+            ]],
+        ];
+    }
+}
